8bc838100e577e01c4fff8115c2a059c9209182e
Kimberly Baker <bakerk2010@gmail.com>
2020-10-28 22:33:08 +0000

    add bin dir and exe

---
+++ environment.rb
@@ -0,0 +1,7 @@
+require_relative "./lib/api.rb"
+require_relative "./lib/cli.rb"
+require_relative "./lib/library.rb"
+
+require "pry"
+require "met/http"   # allows us to make requests from api
+require "json"    # allows us to parse the data we get from api
