847c8e3efb0bd14c1a7a0c21ffed6e4c5959cf40
Kimberly Baker <bakerk2010@gmail.com>
2020-11-01 22:55:26 +0000

    Add instance to exe file.

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,10 @@
 class API
   
-  
-  
+  def self.fetch_libraries
+      url = "https://www.fairfaxcounty.gov/library/branches"
+      binding.pry
+      url = URI(url)
+      response = Net::HTTP.get(uri)
+  end
+      
 end
