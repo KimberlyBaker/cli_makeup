c402344aa314fc2abeeb945a98c74a68ee12adac
Kimberly Baker <bakerk2010@gmail.com>
2020-11-02 02:15:51 +0000

    Add information to api and cli files.

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,19 @@
 class API
   
-  def self.fetch_makeup
+  def self.fetch_makeups
       url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl"
       binding.pry
       uri = URI(url)
       response = Net::HTTP.get(uri)
       hash = JSON.parse(response)
+      
+      array_of_makeups = hash["makeups"]
+      
+      # name
+      # price
+      # description
+      # product type
+      # :name, :price, :description, :product_type
   end
       
 end
