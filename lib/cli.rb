7e10f953dce4b28e288447f47ad6fa7f0d2f7810
Kimberly Baker <bakerk2010@gmail.com>
2020-11-02 04:34:53 +0000

    Add code to cli file.

--- lib/cli.rb
+++ lib/cli.rb
@@ -37,7 +37,26 @@ class CLI
     puts "Enter the number of the makeup product you'd like to know more about."
     index = gets.strip.to_i - 1
     
+    # index valid? number between 0 and 13
+    until index.between?(0, Makeup.all.length -1)
+      # keep asking for user input
+      puts "Sorry, invalid input. Choose a valid number."
+      index = gets.strip.to_i - 1
+    end
+    
     makeup_instance = Makeup.all[index]
+    
+    display_makeup_details(makeup_instance)
+    
+    
+  end
+  
+  def display_makeup_details(makeup)
+    sleep(1)
+    puts "\n"
+    puts makeup.name
+    puts "Produc Type: " + makeup.product_type
+    puts "Description: " + makeup.description
   end
   
 end
