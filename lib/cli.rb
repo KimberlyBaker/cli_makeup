38ad88bbbc4b3850cd9f6ba57fe0abb2d3734f9a
Kimberly Baker <bakerk2010@gmail.com>
2020-11-02 22:35:11 +0000

    Change to cli file.

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,13 +11,14 @@ class CLI
     puts "Would you like to see the list of Cover Girl makeup?"
     puts "Type 'yes' to continue or any another key to exit."
     
-    user_input = gets.strips.downcase
+    user_input = gets.strip.downcase
     
     # if user says yes
     if user_input == "yes" || user_input == "y"
+      puts "\n"
       puts "Good choice!"
-      # display the list
-      display_list_of_brand
+      # display the list of brands
+      list_of_brands
       ask_user_for_brand_choice
       
       sleep(2)
@@ -30,35 +31,35 @@ class CLI
     
   end
   
-  def display_list_of_makeup
-    # access all types of makeup
+  def list_of_brands
+    # access all types of makeup brands
     # binding.pry
     # print each one out
     Makeup.all.each.with_index(1) do |brand, index|
-      puts "#{index}. #{brand.name}"
+      puts "#{index}. #{brand}"
     end
   end
   
-  def ask_user_for_makeup_choice
+  def ask_user_for_brand_choice
     # ask user for choice
     puts "Enter the number of the makeup product you'd like to know more about."
     index = gets.strip.to_i - 1
     
-    # index valid? number between 0 and 13
-    until index.between?(0, Makeup.all.length - 1)
+    # index valid? number between 0 and 19
+    until index.between?(0, brand.all.length - 1)
       # keep asking for user input
       puts "Sorry, invalid input. Choose a valid number."
       index = gets.strip.to_i - 1
     end
     
-    brand_instance = Makeup.all[index]
+    brand_instance = brand.all[index]
     
-    display_makeup_details(brand_instance)
+    display_brand_details(brand_instance)
     
     
   end
   
-  def display_makeup_details(brand)
+  def display_brand_details(brand)
     sleep(1)
     puts "\n"
     puts brand.name
